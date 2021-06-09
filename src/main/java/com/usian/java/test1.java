e5d1dbf95837d3ff14f048fd0197c4c8efe0b58a
xinchao <1917193978@qq.com>
2021-06-09 16:50:26 +0800

    测试项目发布

--- src/main/java/com/usian/java/test1.java
+++ src/main/java/com/usian/java/test1.java
@@ -2,9 +2,6 @@ package com.usian.java;
 
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("师姐你好");
+        System.out.println("我是qq");
     }
 }
-
-
-
--- src/test/java/com/usian/test/test2.java
+++ src/test/java/com/usian/test/test2.java
@@ -1,4 +1,7 @@
 package com.usian.test;
 
 public class test2 {
+    public static void main(String[] args) {
+        System.out.println("woshiaa");
+    }
 }
